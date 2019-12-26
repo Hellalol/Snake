48367e2defbb1bdc5616e06719bf3abb39a31dab
Fredrik Billing <frhabi@gmail.com>
2019-12-26 20:08:56 +0100

    Initial commit

---
+++ src/Snake.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class Snake extends JFrame{
+
+    private final int B_WIDTH = 300;
+    private final int B_HEIGHT = 300;
+    private final int DOT_SIZE = 300;
+    private final int ALL_DOTS = 900;
+    private final int RAND_POS = 29;
+    private final int DELAY = 140;
+
+
+    private final int x[] = new int[ALL_DOTS];
+    private final int y[] = new int[ALL_DOTS];
+
+    private void loadImages(){
+
+        ImageIcon idd = new ImageIcon("dot.png");
+        ball.
+    }
+
+
+
+
+}
